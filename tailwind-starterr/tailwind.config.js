cffc0f7b9eb8c4185441b8fe303e59ac7c957877
Bobade <M.Bobade@serrala.com>
2025-06-27 09:28:07 +0530

    updated

--- tailwind-starterr/tailwind.config.js
+++ tailwind-starterr/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts}"],
+  content: ["./index.html", "./src/**/*.{html,js,ts}"],
   theme: {
     extend: {},
   },
